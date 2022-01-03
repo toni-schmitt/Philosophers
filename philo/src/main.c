67093756acc3842627d4492f350ad9651c6ad585
Toni Schmitt <tonischmitt@online.de>
2022-01-03 21:46:32 +0100

    add argument protection

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 03:15:48 by toni              #+#    #+#             */
-/*   Updated: 2022/01/03 03:16:12 by toni             ###   ########.fr       */
+/*   Updated: 2022/01/03 21:46:26 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int	main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	return (0);
+	if (argc < 5 || argc > 6)
+	{
+		printf("Usage: %s [no_of_philos] [time_to_die] [time_to_eat] [time_to_sleep] (no_of_min_meals)\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
