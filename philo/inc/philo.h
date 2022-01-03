98ca278b6adf787b5bd8424a1d22a0cd120d38c1
Toni Schmitt <tonischmitt@online.de>
2022-01-03 03:40:56 +0100

    add default include headers

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,11 +6,19 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 03:15:31 by toni              #+#    #+#             */
-/*   Updated: 2022/01/03 03:15:45 by toni             ###   ########.fr       */
+/*   Updated: 2022/01/03 03:33:22 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+// INCLUDES
+# include <stdlib.h>
+# include <stdbool.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 #endif
