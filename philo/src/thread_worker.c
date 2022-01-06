385c9177c6e37e2aab02b841e1d230e833259371
Toni Schmitt <tonischmitt@online.de>
2022-01-06 19:30:25 +0100

    thread worker now returns failure on death

--- philo/src/thread_worker.c
+++ philo/src/thread_worker.c
@@ -6,7 +6,7 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 23:58:39 by toni              #+#    #+#             */
-/*   Updated: 2022/01/06 18:09:24 by toni             ###   ########.fr       */
+/*   Updated: 2022/01/06 19:27:53 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,7 +109,7 @@ void	detach_threads(t_philo *philos, uint no_of_philos)
 	}
 }
 
-static void	check_dead(t_data *data)
+static size_t	check_dead(t_data *data)
 {
 	uint	i;
 
@@ -125,7 +125,7 @@ static void	check_dead(t_data *data)
 					data->philo_died = true;
 					join_threads(data->philos_data);
 					philo_print("is dead", i + 1);
-					return ;
+					return (EXIT_FAILURE);
 				}
 			}
 			i++;
@@ -134,10 +134,11 @@ static void	check_dead(t_data *data)
 		{
 			join_threads(data->philos_data);
 			// detach_threads(data->philos_data, data->prog_args[no_of_philos]);
-			return ;
+			return (EXIT_SUCCESS);
 		}
 	}
 	join_threads(data->philos_data);
+	return (EXIT_FAILURE);
 }
 
 void	*thread_woker(void *arg)
@@ -151,6 +152,5 @@ void	*thread_woker(void *arg)
 	while (get_data()->waiting_in_queue != get_data()->prog_args[no_of_philos])
 		;
 	philo_thread_sleep_ms(get_data()->prog_args[time_to_die] / 2);
-	check_dead(data);
-	return (NULL);
+	return ((void *)check_dead(data));
 }
